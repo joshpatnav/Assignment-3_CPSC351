a00c3e0bc06f84eec6b5a8cdc036943177ddb7af
joshpatnav <143125343+joshpatnav@users.noreply.github.com>
2025-11-24 06:25:46 +0000

    Fully finsihed

--- Q1_Pthread_Mutex.cpp
+++ Q1_Pthread_Mutex.cpp
@@ -1,5 +1,5 @@
 /*
-This program is meant to count the threads with the mutex protections
+This program is meant to increment the global var across 10 threads with the protection from mutex
 Compile: g++ -std=c++17 -pthread -O2 Q1_Pthread_Mutex.cpp -o with_mutex
 Run: ./with_mutex
 */
@@ -10,32 +10,32 @@ const int THREADS = 10;
 const int ITERS = 100000;
 
 int counter = 0; // Global Var
-pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock;
 
 void * worker(void* arg) {
     (void)arg;
-    for (int i = 0; i < ITERS; ++i) 
+    for (int i = 0; i < ITERS; i++) 
     {
-        pthread_mutex_lock(&counter_mutex);
+        pthread_mutex_lock(&lock);
         counter++;
-        pthread_mutex_unlock(&counter_mutex);
+        pthread_mutex_unlock(&lock);
     }
-    return nullptr;
+    return NULL;
 }
 
 int main() {
     pthread_t threads[THREADS];
 
-    for (int i = 0; i < THREADS; ++i) {
-        pthread_create(&threads[i], nullptr, worker, nullptr);
-    }
-    
-    for (int i = 0; i < THREADS; ++i) {
-        pthread_join(threads[i], nullptr);
-    }
+    pthread_mutex_init(&lock, NULL);
+
+    //Create 10 threads
+    for (int i = 0; i < THREADS; i++) pthread_create(&threads[i], NULL, worker, NULL);
+    //Each thread increments
+    for (int i = 0; i < THREADS; i++) pthread_join(threads[i], NULL);
+
+    pthread_mutex_destroy(&lock);
 
     std::cout << "Final counter(With Mutex): " << counter << " (expected " << THREADS * ITERS << ")\n";
 
-    pthread_mutex_destroy(&counter_mutex);
     return 0;
 }
--- Q1_Pthread_NoMutex.cpp
+++ Q1_Pthread_NoMutex.cpp
@@ -1,5 +1,5 @@
 /*
-This program is to count the threads without the protection of the mutex
+This program is meant to increment the global var across 10 threads without the protection from mutex
 Compile: g++ -std=c++17 -pthread -O2 Q1_Pthread_NoMutex.cpp -o no_mutex
 Run: ./no_mutex
 */
@@ -13,24 +13,20 @@ int counter = 0; // Global Var
 
 void * worker(void* arg) {
     (void)arg;
-    for (int i = 0; i < ITERS; ++i) {
+    for (int i = 0; i < ITERS; i++) {
         counter++;
     }
-    return nullptr;
+    return NULL;
 }
 
 int main() {
     pthread_t threads[THREADS];
 
     // Create the 10 threads
-    for (int i = 0; i < THREADS; ++i) {
-        pthread_create(&threads[i], nullptr, worker, nullptr);
-    }
+    for (int i = 0; i < THREADS; i++) pthread_create(&threads[i], NULL, worker, NULL);
     
     // Wait for all of the threads to finish
-    for (int i = 0; i < THREADS; ++i) {
-        pthread_join(threads[i], nullptr);
-    }
+    for (int i = 0; i < THREADS; i++) pthread_join(threads[i], NULL);
 
     std::cout << "Final counter(Without Mutex): " << counter << " (expected " << THREADS * ITERS << ")\n";
     return 0;
